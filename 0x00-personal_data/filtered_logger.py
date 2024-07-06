c5a0596a3f29c47b17042a1e43eb9d8cecbcb94f
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-07 00:49:22 +0300

    t0v1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+this is log message obfuscated files
+"""
+
+
+from typing import List
+import re
+import logging
+from os import environ
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces sensitive information in a message
+    Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+
+    Returns:
+        The filtered string message with redacted values
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
