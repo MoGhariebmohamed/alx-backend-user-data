aa3e7174464a1152820584ccdb1e86dc9d81e654
Mhamed Gharieb <m.gharieb89@gmail.com>
2024-07-13 04:18:46 +0300

    t0v1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,83 +1,68 @@
-
 #!/usr/bin/env python3
 """
-Basic flask application for User Authentication Service
+Route module for the API
 """
-from auth import Auth
-from flask import Flask, jsonify, request, abort, redirect
-from sqlalchemy.orm.exc import NoResultFound
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
-AUTH = Auth()
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
-@app.route("/", strict_slashes=False)
-def get_message():
-    """Get message"""
-    return jsonify({"message": "Bienvenue"})
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users():
-    """Register a user"""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError as err:
-        return jsonify({"message": "email already registered"}), 400
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login():
-    """Login user"""
-    try:
-        email = request.form.get("email")
-        password = request.form.get("password")
-    except KeyError:
-        abort(400)
-
-    try:
-        if AUTH.valid_login(email, password):
-            session_id = AUTH.create_session(email)
-            response = jsonify({"email": email, "message": "logged in"})
-            response.set_cookie("session_id", session_id)
-            return response
-        else:
-            abort(401)
-    except NoResultFound:
-        abort(401)
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout():
-    """Logout user"""
-    session_id = request.cookies.get("session_id", None)
+@app.before_request
+def authenticate_user():
+    """Authenticates a user before processing a request.
+    """
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
-    if session_id is None:
-        abort(403)
 
-    user = AUTH.get_user_from_session_id(session_id)
-
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect("/")
-    else:
-        abort(403)
-
-
-@app.route("/profile", strict_slashes=False)
-def profile():
-    """Get user profile"""
-    session_id = request.cookies.get("session_id", None)
-
-    if session_id is None:
-        abort(403)
-
-    user = AUTH.get_user_from_session_id(session_id)
-
-    if user:
-        return jsonify({"email": user.email}), 200
-    abort(403)
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
